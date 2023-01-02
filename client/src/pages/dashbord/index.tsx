1ed7a00009a63195218f48a85387a07ef273fe93
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2023-01-02 17:25:02 -0500

    finish Home page skeleton

--- client/src/pages/dashbord/index.tsx
+++ client/src/pages/dashbord/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar';
 
 const Home = () => {
@@ -7,8 +8,17 @@ const Home = () => {
       <Navbar />
 
       <main className='w-full px-8 py-4'>
-        <section>Document</section>
-        <section></section>
+        <section className='w-4/5 h-60 m-auto flex gap-4'>
+          <Link to='/create' className='w-48 h-full bg-blue-800 flex items-center justify-center rounded-md'>Create Doc</Link>
+          <Link to='/create' className='w-48 h-full bg-blue-800 flex items-center justify-center rounded-md'>Create Doc</Link>
+          <Link to='/create' className='w-48 h-full bg-blue-800 flex items-center justify-center rounded-md'>Create Doc</Link>
+          <Link to='/create' className='w-48 h-full bg-blue-800 flex items-center justify-center rounded-md'>Create Doc</Link>
+        </section>
+
+        <section className='w-4/5 h-96 m-auto mt-8 flex gap-4'>
+          <Link to='/' className='w-1/2 flex h-full bg-blue-900 rounded-md'></Link>
+          <Link to='/' className='w-1/2 flex h-full bg-blue-900 rounded-md'></Link>
+        </section>
       </main>
     </div>
   )
