d8ce971500246505dc48bc8f737805e08f3146df
cliford-dareus <cliforddareus@gmail.com>
2023-01-15 19:24:56 -0500

    Add quill editor

--- client/src/pages/create/index.tsx
+++ client/src/pages/create/index.tsx
@@ -1,42 +1,41 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { BsTypeBold, BsTypeItalic } from 'react-icons/bs';
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
 
 const CreateDoc = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [ isInEditMode, setIsInEditMode ] = useState<Boolean>(true);
-  const [ formatter, setFormatter ] = useState<string>('');
-  const [ data, setData ] = useState<string | undefined>('');
-
-  const startTyping = (event: any) => {
-    const textNode = document.querySelector('.editor');
-    setData(event?.target?.innerHTML);
-    console.log(event.target)
-    // div?.append(node);
-  };
-
+  const [value, setValue] = useState<string>("");
+  const editorRef = useRef<ReactQuill>(null);
+  const editor = editorRef.current?.getEditor();
+  const editorPriv = editorRef.current?.makeUnprivilegedEditor(editor!);
+  console.log(editorPriv?.getHTML())
   return (
-    <div className='bg-black h-full'>
-      <div className='w-full py-4 px-8'>
-        <Link to='/' className='text-2xl text-white'>SNEEKDOCS</Link>
+    <div className="bg-black h-full">
+      <div className="w-full py-4 px-8">
+        <Link to="/" className="text-2xl text-white">
+          SNEEKDOCS
+        </Link>
       </div>
 
-      <main className='px-8 py-4 h-5/6'>
-        {/* Format toolbar */}
-        <div className='w-3/4 h-8 flex items-center justify-center gap-4 m-auto bg-white border-b border-black'>
-            <div onClick={() => setFormatter('b')}> <BsTypeBold /> </div>
-            <div onClick={() => setFormatter('i')}> <BsTypeItalic /> </div>
-            <div> <BsTypeBold /> </div>
-            <div> <BsTypeBold /> </div>
-            <div> <BsTypeBold /> </div>
-        </div>
-
-        {/* Text editor  need word wraps too */}
-        <div ref={ref} contentEditable className='editor w-3/4 m-auto bg-white h-full overflow-auto p-3'>
-        </div>
+      <main className="p-4 h-5/6 bg-white">
+        <ReactQuill
+          ref={editorRef}
+          theme={"snow"}
+          value={value}
+          onChange={setValue}
+          style={{ width: "100%", height: "90%" }}
+        />
       </main>
+
+      <ReactQuill
+        theme="bubble"
+        value={editorPriv?.getHTML()}
+        readOnly
+        style={{color: 'white'}}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default CreateDoc;
